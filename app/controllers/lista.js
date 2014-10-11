dc02945eefef15fe8b608e2519d44cc5ba37649d
kokeloker <kokeloker@gmail.com>
2014-10-11 12:46:41 -0300

    5

--- app/controllers/lista.js
+++ app/controllers/lista.js
@@ -27,6 +27,7 @@ for (var i=0, ilen=people.length; i<ilen; i++){
 				color: '#000'
 			});
 			
+			/*
 			var check = Ti.UI.createSwitch({
 				type: Titanium.UI.Android.SWITCH_STYLE_CHECKBOX,
 				width: "45",
@@ -52,14 +53,42 @@ for (var i=0, ilen=people.length; i<ilen; i++){
 			});
 			
 			*/
+			var checkbox = createCheckbox({});
 			
 			vista.add(nombreContacto);
-			vista.add(check);
+			//vista.add(check);
+			vista.add(checkbox);
 			newContact.add(vista);
 			$.contactList.appendRow(newContact);
 		}
 	}
 };
+
+function createCheckbox (specs) {
+    if(typeof specs != "object")
+        specs = {};
+    specs.width = specs.width || 45;
+    specs.backgroundColor = specs.unCheckedColor || "white";
+    specs.height = specs.height || 45;
+    specs.border = specs.border || 5;
+    specs.right =  specs.right || "3%";
+    specs.borderColor = specs.borderColor || "silver";
+    var viw = Ti.UI.createView(specs);
+
+    function togglecheck () {
+        if(!viw.checked) {
+            viw.checked = true;
+            viw.backgroundColor = specs.checkedColor || "green";
+        }
+        else {
+            viw.checked = false;
+            viw.backgroundColor = specs.unCheckedColor || "white";
+        }           
+    }
+    viw.addEventListener("click",togglecheck);
+
+    return viw;
+}
 
 $.ventana.open();
 
