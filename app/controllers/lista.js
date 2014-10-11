a3f80216f0970a820b02251d64841a86eea56335
kokeloker <kokeloker@gmail.com>
2014-10-11 12:25:38 -0300

    3

--- app/controllers/lista.js
+++ app/controllers/lista.js
@@ -11,52 +11,52 @@ for (var i=0, ilen=people.length; i<ilen; i++){
 	if (JSON.stringify(person[multiValue[1]]) != '{}'){
 		if (JSON.stringify(person[multiValue[0]]) != '{}'){
 			Ti.API.info('phone:' + JSON.stringify(person[multiValue[1]]));
+		
+			var newContact = Ti.UI.createTableViewRow();
+			
+			var vista = Ti.UI.createView({
+				borderColor: "#afafaf",
+				borderRadius: "5",
+				height: "40",
+				backgroundColor:'#e7e9e7',	
+			});	
+			
+			var nombreContacto = Ti.UI.createLabel({
+				text: person[singleValue[1]],
+				left:'5%',
+				color: '#000'
+			});
+			
+			var check = Ti.UI.createSwitch({
+				type: Titanium.UI.Android.SWITCH_STYLE_CHECKBOX,
+				width: "35",
+				height: "35",
+				right: "3%",
+				borderColor: "#afafaf",
+				borderRadius: "5",
+				backgroundColor: "#fff",
+				color: "#fff",
+				value: "false"
+			});
+			
+			
+			 check.addEventListener('change', function(e){
+				Ti.API.info(check.value);
+				if(check.value == true){
+					check.backgroundColor = "#fff";
+					check.color = "#fff";
+				} else{
+					check.backgroundColor = "#0071bc";
+					check.color = "#0071bc";
+				}	
+			});
+			
+			vista.add(nombreContacto);
+			vista.add(check);
+			newContact.add(vista);
+			$.contactList.appendRow(newContact);
 		}
 	}
-	var newContact = Ti.UI.createTableViewRow();
-	
-	var vista = Ti.UI.createView({
-		borderColor: "#afafaf",
-		borderRadius: "5",
-		height: "40",
-		backgroundColor:'#e7e9e7',	
-	});	
-	
-	var nombreContacto = Ti.UI.createLabel({
-		text: person[singleValue[1]],
-		left:'5%',
-		color: '#000'
-	});
-	
-	var check = Ti.UI.createSwitch({
-		type: Titanium.UI.Android.SWITCH_STYLE_CHECKBOX,
-		width: "35",
-		height: "35",
-		right: "3%",
-		borderColor: "#afafaf",
-		borderRadius: "5",
-		backgroundColor: "#fff",
-		color: "#fff"
-	});
-	
-	/*
-	 check.addEventListener('click', function(e){
-		Ti.API.info('clicked');
-		if(check.value == true){
-			check.value = false;
-			check.backgroundColor = "#fff";
-			check.color = "#fff";
-		} else{
-			check.value = true;
-			check.backgroundColor = "#0071bc";
-			check.color = "#0071bc";
-		}	
-	});
-	*/	
-	vista.add(nombreContacto);
-	vista.add(check);
-	newContact.add(vista);
-	$.contactList.appendRow(newContact);
 };
 
 $.ventana.open();
